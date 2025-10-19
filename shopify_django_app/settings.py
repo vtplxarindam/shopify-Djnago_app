e0802b79e8d3b63663debfdfeab5a1657917c462
attheratearindam <mr.arindamkhan@yahoo.com>
2025-10-19 22:13:59 +0530

    settings.py files updated

--- shopify_django_app/settings.py
+++ shopify_django_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from shopify_app import *
-from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,5 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
